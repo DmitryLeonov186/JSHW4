2fd2d5956a10181e924fd7351ebca26ab79304c1
Dmitry Leonov <lemomonster@mail.ru>
2025-04-24 23:19:38 +0500

    ДЗ доработано

--- gh.test.js
+++ gh.test.js
@@ -2,7 +2,6 @@ let page;
 
 beforeEach(async () => {
   page = await browser.newPage();
-  await page.goto("https://github.com/team");
 });
 
 afterEach(() => {
@@ -10,6 +9,9 @@ afterEach(() => {
 });
 
 describe("Github page tests", () => {
+  beforeEach(async () => {
+    await page.goto("https://github.com/team");
+}, 60000);
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
@@ -18,13 +20,13 @@ describe("Github page tests", () => {
     const expected =
       "GitHub · Build and ship software on a single, collaborative platform · GitHub";
     expect(actual).toEqual(expected);
-  });
+  }, 60000);
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", (link) => link.getAttribute("href"));
     const expected = "#start-of-content";
     expect(actual).toEqual(expected);
-  });
+  }, 60000);
 
   test("The page contains Sign in button", async () => {
     const btnSelector = ".btn-large-mktg.btn-mktg";
@@ -34,7 +36,7 @@ describe("Github page tests", () => {
     const actual = await page.$eval(btnSelector, (link) => link.textContent);
     const expected = "Get started with Team";
     expect(actual).toContain(expected);
-  });
+  }, 60000);
 });
 
 describe("Task 2", () => {
@@ -43,14 +45,14 @@ describe("Task 2", () => {
     const actual = await page.title();
     const expected = "Home - The GitHub Blog";
     expect(actual).toContain(expected);
-  });
+  }, 60000);
 
   test("Test 2", async () => {
     await page.goto("https://github.com/pricing");
     const actual = await page.title();
     const expected = "Pricing · Plans for every developer · GitHub";
     expect(actual).toContain(expected);
-  });
+  }, 60000);
 
   test("Test 3", async () => {
     await page.goto("https://github.com/security/advanced-security");
@@ -58,5 +60,5 @@ describe("Task 2", () => {
     const actual = await page.$eval(h1, (el) => el.textContent);
     const expected = "Security that moves at the  speed of development";
     expect(actual).toEqual(expected);
-  });
+  }, 60000);
 });
--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,4 @@ module.exports = {
   verbose: true, //указывает на то, что каждый тест будет показан в отчете о процессе запуска
   preset: "jest-puppeteer", //указываем, что будем использовать эту библиотеку
   maxWorkers: 1,
-  testTimeout: 60000
 };
