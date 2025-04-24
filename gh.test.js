89bb76779e0958ed2c9e00496832d1f818ade68b
Dmitry Leonov <lemomonster@mail.ru>
2025-04-24 10:55:50 +0500

    Task 1

---
+++ gh.test.js
@@ -0,0 +1,35 @@
+let page;
+
+beforeEach(async () => {
+  page = await browser.newPage();
+  await page.goto("https://github.com/team");
+});
+
+afterEach(() => {
+  page.close();
+});
+
+describe("Github page tests", () => {
+  test("The h1 header content'", async () => {
+    jest.setTimeout(10000)
+    const firstLink = await page.$("header div div a");
+    await firstLink.click();
+    await page.waitForSelector('h1');
+    const title2 = await page.title();
+    expect(title2).toEqual('GitHub · Build and ship software on a single, collaborative platform · GitHub');
+  }, 60000);
+
+  test("The first link attribute", async () => {
+    const actual = await page.$eval("a", link => link.getAttribute('href') );
+    expect(actual).toEqual("#start-of-content");
+  }, 60000);
+
+  test("The page contains Sign in button", async () => {
+    const btnSelector = ".btn-large-mktg.btn-mktg";
+    await page.waitForSelector(btnSelector, {
+      visible: true,
+    });
+    const actual = await page.$eval(btnSelector, link => link.textContent);
+    expect(actual).toContain("Get started with Team")
+  }, 60000);
+});
